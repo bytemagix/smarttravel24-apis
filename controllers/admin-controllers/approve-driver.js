db419ce8697bad7d2d8217022a8ca16ce559949a
Pranjal Das <bytemagicsoftwares@gmail.com>
2021-09-25 10:52:03 +0530

    Approve Driver API added

---
+++ controllers/admin-controllers/approve-driver.js
@@ -0,0 +1,33 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("../../config/smarttravel24-c8fad-firebase-adminsdk-erorc-d149407dfe.json");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://smarttravel24-c8fad-default-rtdb.firebaseio.com",
+    storageBucket: "smarttravel24-c8fad.appspot.com",
+  });
+}
+
+exports.approveDriver = async (req, res) => {
+  try {
+    const formData = req.fields;
+
+    const driverId = formData.driverId;
+    const verificationStatus = formData.verificationStatus;
+
+    const db = admin.database();
+    db.ref("Drivers").child("Profiles").child(driverId).child("status").update({
+        verificationStatus : verificationStatus,
+    });
+
+
+    res.status(200).json({
+      message: "OK",
+    });
+  } catch (err) {
+    res.status(400).json({
+      error: err,
+    });
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -5,10 +5,12 @@ const addCarTypeController = require("../controllers/admin-controllers/add-car-t
 const addCarNameController = require("../controllers/admin-controllers/add-car-name");
 const deleteCarTypeController = require("../controllers/admin-controllers/delete-car-type");
 const deleteCarNameController = require("../controllers/admin-controllers/delete-car-name");
+const approveDriverController = require("../controllers/admin-controllers/approve-driver");
 
 router.post('/add-car-type', addCarTypeController.addCarType);
 router.post('/add-car-name', addCarNameController.addCarName);
 router.post('/delete-car-type',deleteCarTypeController.deleteCarType);
 router.post('/delete-car-name', deleteCarNameController.deleteCarName);
+router.post('/approve-driver',approveDriverController.approveDriver);
 
 module.exports = router;
